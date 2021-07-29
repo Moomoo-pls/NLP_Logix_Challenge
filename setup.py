fd06c3872fa84dfff8d836e8c9626ddffd98fa53
Moomoo-pls <mooyoung12@gmail.com>
2021-07-29 18:56:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 import setuptools
 from setuptools import version
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="Moo_NLP_Challenge",
     version="1.0.0",
     author="Stephen Moo-Young",
     author_email="mooyoung12@gmail.com",
     description="Challenges 1, 4, and 5 for the take home coding challenge",
-    long_description=long_description,
     url="https://github.com/Moomoo-pls/NLP_Logix_Challenge",
     packages=setuptools.find_packages(),
     entry_points={
